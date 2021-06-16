09d979291ee8dee614f61305ddd041a5f5b51e1e
Uday Bhaskar <44165541+justUB@users.noreply.github.com>
2021-06-16 13:13:14 +0530

    Update store_routes.js

--- main/routes/store_routes.js
+++ main/routes/store_routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
-import { getProducts,getProductById,addCart } from '../controllers/product_controller.js';
+import { getProducts,getProductById,addCart, getCart} from '../controllers/product_controller.js';
 const store_router = express.Router();
 
 store_router.get('/', getProducts);
+store_router.get('/cart/:name',getCart);
 store_router.get('/:id',getProductById);
 store_router.post('/:id',addCart);
+
 
 export default store_router;
